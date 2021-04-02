08c1b3bc39b97284f034cb22a9df26076cc997ec
salil-sudo <salil@salil.cool>
2021-04-02 11:56:22 +0200

    meh

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f4eac7", "#0B0D2A" },
-	[SchemeSel] = { "#f4eac7", "#BD6D35" },
-	[SchemeOut] = { "#f4eac7", "#FBCD63" },
-};
--- .scripts/spotifyinfo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/home/salil/.scripts/reload_dunst.sh
-notify-send "$(spotifycli --song)" "$(spotifycli --artist)"
